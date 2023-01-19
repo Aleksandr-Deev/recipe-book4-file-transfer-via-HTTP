e7642abcbe666a62814730e0b7b9a36883196e65
Aleksandr Deev <geshtait@mail.ru>
2023-01-19 21:21:36 +0500

    Первый коммит

---
+++ src/main/java/com/example/recipebook4filetransferviahttp/RecipeBook4FileTransferViaHttpApplication.java
@@ -0,0 +1,13 @@
+package com.example.recipebook4filetransferviahttp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeBook4FileTransferViaHttpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeBook4FileTransferViaHttpApplication.class, args);
+    }
+
+}
